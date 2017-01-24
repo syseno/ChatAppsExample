4318b2b8c247282ba0589943e1b582b776a8b9b7
Singgih Harseno <Singgih Harseno>
2017-01-24 10:23:11 +0700

    Adding documentation folder

--- app/src/main/java/com/example/singgih/chatappsexample/Main/MainActivity.java
+++ app/src/main/java/com/example/singgih/chatappsexample/Main/MainActivity.java
@@ -1,10 +1,6 @@
 package com.example.singgih.chatappsexample.Main;
 
-import android.content.Context;
 import android.os.Bundle;
-import android.view.View;
-import android.view.WindowManager;
-import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.ListView;
 
